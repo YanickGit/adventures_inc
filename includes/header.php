f9a0e493fc0d1e173494888bb3ba4fd88da63996
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-17 06:09:37 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -49,14 +49,18 @@
 
   <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="index.php">IT Conference</a>
+      <a class="navbar-brand" href="index.php">Adventures Inc</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav mr-auto">
           <a class="nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
-          <a class="nav-link" href="viewrecords.php">View Attendees</a>
+          <a class="nav-link" href="#">Adventures</a>
+          <a class="nav-link" href="#">The Explorers</a>
+          <a class="nav-link" href="#">Register</a>
+          <a class="nav-link" href="#">About Us</a>
+          <a class="nav-link" href="#">Contact Us</a>
         </div>
         <div class="navbar-nav ml-auto">
             <?php 
