97cad53f4601772514105db1bfc6cfaacc08c69e
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-17 06:12:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $title = 'Home';
     require_once 'includes/header.php';
     require_once 'includes/slider.php';
-    require_once 'db/db_connect.php';
+    //require_once 'db/db_connect.php';
 ?>
 
 <h1 class = "text-center">Home Page</h1>
