804bc42c62f4ade93b91f5e03b155739836c3eaa
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-17 06:13:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     $title = 'Home';
     require_once 'includes/header.php';
-    require_once 'includes/slider.php';
+    //require_once 'includes/slider.php';
     //require_once 'db/db_connect.php';
 ?>
 
