53044b604b2873e9890fa138d58a46fc2f0e46c2
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-11-18 17:16:42 -0500

    Update The Team

--- the-team.php
+++ the-team.php
@@ -10,7 +10,7 @@
   <div class="column">
     <div class="card">
       <img src="images/team/__defaultCrop.jpg" alt="Jane" style="width:100%">
-      <div class="container">
+      <div class="theTeam">
         <h2>Jane Doe</h2>
         <p class="title">CEO & Founder</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
@@ -23,7 +23,7 @@
   <div class="column">
     <div class="card">
       <img src="images/team/__defaultCrop.jpg" alt="Mike" style="width:100%">
-      <div class="container">
+      <div class="theTeam">
         <h2>Mike Ross</h2>
         <p class="title">Art Director</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
@@ -36,7 +36,7 @@
   <div class="column">
     <div class="card">
       <img src="images/team/__defaultCrop.jpg" alt="John" style="width:100%">
-      <div class="container">
+      <div class="theTeam">
         <h2>John Doe</h2>
         <p class="title">Designer</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
@@ -47,46 +47,6 @@
   </div>
 </div>
 
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="images/team/__defaultCrop.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Jane Doe</h2>
-        <p class="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><a href="contact-us.php" class="btn btn-primary">Contact Us</a></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="images/team/__defaultCrop.jpg" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><a href="contact-us.php" class="btn btn-primary">Contact Us</a>></p>
-      </div>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <img src="images/team/__defaultCrop.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><a href="contact-us.php" class="btn btn-primary">Contact Us</a></p>
-      </div>
-    </div>
-  </div>
-</div>
 
 <?php
     require_once 'includes/footer.php';
