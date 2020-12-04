8d32ba5952158f9b900bc5bbdbdc09a0b41bbde1
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-12-04 18:09:49 -0500

    Purge Records

---
+++ action-purge-a-client.php
@@ -0,0 +1,30 @@
+<?php 
+$title = 'Action';
+require_once 'includes/header.php';
+require_once 'includes/auth_check.php';
+require_once 'db/db_connect.php';
+
+if (!$_GET['client_id']){
+    include 'includes/error_message.php';
+}else {
+    //Get ID Values
+    $client_id = $_GET['client_id'];
+
+    //Call Delete Function
+    $delete_result = $client_crud->deleteSoftClients($client_id);
+
+    //Redirect to ViewRecords
+    if ($delete_result){
+        //header("Location: view-all-deleted-clients.php");
+        ?>
+        <script>
+        location.replace("view-all-deleted-clients.php")
+        </script>
+        <?php
+    } else{
+        require_once 'includes/error_message.php';
+    }
+}
+
+require_once 'includes/footer.php'
+?> 
