30076a843c4f19c00102cb997fd8cb1be531471f
YanickGit <46899594+YanickGit@users.noreply.github.com>
2020-12-04 09:38:47 -0500

    Update view-all-current-clients.php

--- view-all-current-clients.php
+++ view-all-current-clients.php
@@ -15,7 +15,7 @@
 <?php include 'includes/view-all-clients-nav.php'; ?>
 <br>
 
-<table class="table table-striped" id="dtBasicExample">
+<table class="table table-striped table-responsive" id="dtBasicExample">
   <thead>
     <tr>
       <th scope="col">#</th>
